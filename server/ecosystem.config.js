217a6a225b3b00d1341e65991023e951a7c85050
chandrasekhar <96806629+ChandraSekhar-1306@users.noreply.github.com>
2025-06-28 20:22:13 +0530

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
         {
             name: "WorkDeck",
-            script:"npm",
-            args: "run dev",
+            script:"npx",
+            args: "ts-node src/index.ts",
             env:{
                 NODE_ENV: "development",
             },
